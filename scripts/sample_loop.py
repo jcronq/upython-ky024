9827e553113f9d658565249a8ecdfdd39bef68b6
Jason Cronquist <jcronq@gmail.com>
2022-10-31 21:03:45 -0400

    averaged reading

--- scripts/sample_loop.py
+++ scripts/sample_loop.py
@@ -6,11 +6,8 @@ import time
 sample_count = 10
 
 while True:
-    readings = []
-    for idx in range(sample_count):
-        response = requests.get("http://10.20.30.139:8008/hall_voltage")
-        time.sleep(0.1)
-        readings.append(int(response.text))
-    print(sum(readings)/sample_count)
+    response = requests.get("http://10.20.30.139:8008/hall_voltage")
+    print(response.text)
+    time.sleep(0.1)
 
         
--- sensor/src/ky024/__init__.py
+++ sensor/src/ky024/__init__.py
@@ -1,8 +1,15 @@
 import machine
+import time
 
 class Controller:
+    reading_count = 10
+
     def __init__(self, ADC_PIN):
         self.adc = machine.ADC(machine.Pin(ADC_PIN))
 
     def get_value(self):
-        return self.adc.read_u16()
+        readings = []
+        for idx in range(self.reading_count):
+            readings.append(self.adc.read_u16())
+            time.sleep(0.1)
+        return sum(readings) / self.reading_count
